f5f9ff4fa5a523979953f2db18c4c5c1cd5b1df8
cl <chilian@517na.com>
2021-07-27 10:41:57 +0800

    info 解答成功: 执行耗时:4 ms,击败了100.00% 的Java用户 内存消耗:38.9 MB,击败了60.76% 的Java用户

--- leetcode/editor/cn/DesignCircularQueue.java
+++ leetcode/editor/cn/DesignCircularQueue.java
@@ -54,10 +54,12 @@ public class DesignCircularQueue {
                 .append("enQueue(1)-").append(solution.enQueue(1)).append("\n****\n")
                 .append("enQueue(10)-").append(solution.enQueue(10)).append("\n****\n")
                 .append("Rear()-").append(solution.Rear()).append("\n****\n")
+                .append("Front()-").append(solution.Front()).append("\n****\n")
                 .append("isFull()-").append(solution.isFull()).append("\n****\n")
                 .append("deQueue()-").append(solution.deQueue()).append("\n****\n")
                 .append("enQueue(4)-").append(solution.enQueue(4)).append("\n****\n")
-                .append("Rear()-").append(solution.Rear()).append("\n****\n");
+                .append("Rear()-").append(solution.Rear()).append("\n****\n")
+                .append("Front()-").append(solution.Front()).append("\n****\n");
         System.out.println(sbf);
     }
     /**
